c33a40d97453898b7f85319cc44d08d53f6408e1
bhanu3023 <bhanuprakash80963@gmail.com>
2024-08-13 11:23:22 +0530

    Update Armstrong.java

--- src/test/java/Armstrong.java
+++ src/test/java/Armstrong.java
@@ -3,7 +3,7 @@ public class Armstrong {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int number = 371, originalNumber, remainder, result = 0;
+		int number = 271, originalNumber, remainder, result = 0;
 
         originalNumber = number;
 
